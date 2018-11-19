96614155427bd5f1ac29c71bbffeb7a4482fc065
Daniel Dias <danieldias15@gmail.com>
2018-11-19 03:42:23 -0500

    Bare bones EQ visualizer complete

--- puis-audio-visualizer/src/app/visualizer-container/visualizer-container.component.ts
+++ puis-audio-visualizer/src/app/visualizer-container/visualizer-container.component.ts
@@ -63,6 +63,159 @@ export class VisualizerContainerComponent implements AfterViewInit {
     @ViewChild("visualizers")
     canvasRef: ElementRef;
 
+    private waveFunc = x => Math.sin(x * Math.PI);
+    private waveFuncNeg = x => - 4*x + 4*x*x; // 1 - (2*x - 1) * (2*x - 1)
+    private CIRCLES = [
+        {
+            radius : 3.6,
+            thickness : 0.08
+        },
+        {
+            radius : 2.6,
+            thickness : 0.065
+        },
+        {
+            radius : 1.8,
+            thickness : 0.045
+        },
+        {
+            radius : 1.25,
+            thickness : 0.04
+        },
+        {
+            radius : 0.8,
+            thickness : 0.04
+        },
+        {
+            radius : 0.42,
+            thickness : 0.04
+        }
+    ];
+    private WAVES = {
+        freq : [
+            {
+                step : 6.0 * Math.PI / 12 + 0.2,
+                wave : this.waveFunc,
+                amplitude : 0.72,
+                startAngle : null,
+                radius : 3.6,
+                arcLength : 6.6 * Math.PI / 12,
+                displayThickness : 0.08,
+                resolution : 32
+            },
+            {
+                step : 2.8 * Math.PI / 12 - 0.04,
+                wave : this.waveFuncNeg,
+                amplitude : 0.56,
+                startAngle : null,
+                radius : 3.6,
+                arcLength : 2.8 * Math.PI / 12,
+                displayThickness : 0.07,
+                resolution : 32
+            },
+            {
+                step : 4.1 * Math.PI / 12 - 0.12,
+                wave : this.waveFunc,
+                amplitude : 0.48,
+                startAngle : null,
+                radius : 2.6,
+                arcLength : 4.1 * Math.PI / 12,
+                displayThickness : 0.06,
+                resolution : 32
+            },
+            {
+                step : 2.5 * Math.PI / 12,
+                wave : this.waveFuncNeg,
+                amplitude : 0.42,
+                startAngle : null,
+                radius : 2.6,
+                arcLength : 2.5 * Math.PI / 12,
+                displayThickness : 0.05,
+                resolution : 32
+            },
+            {
+                step : 2.8 * Math.PI / 12,
+                wave : this.waveFunc,
+                amplitude : 0.45,
+                startAngle : null,
+                radius : 1.8,
+                arcLength : 2.8 * Math.PI / 12,
+                displayThickness : 0.04,
+                resolution : 24
+            },
+            {
+                step : 1.5 * Math.PI / 12,
+                wave : this.waveFuncNeg,
+                amplitude : 0.32,
+                startAngle : null,
+                radius : 1.8,
+                arcLength : 1.5 * Math.PI / 12,
+                displayThickness : 0.04,
+                resolution : 24
+            },
+            {
+                step : 1.2 * Math.PI / 12,
+                wave : this.waveFunc,
+                amplitude : 0.3,
+                startAngle : null,
+                radius : 1.25,
+                arcLength : 1.2 * Math.PI / 12,
+                displayThickness : 0.04,
+                resolution : 16
+            },
+            {
+                step : 0.85 * Math.PI / 12,
+                wave : this.waveFuncNeg,
+                amplitude : 0.27,
+                startAngle : null,
+                radius : 1.25,
+                arcLength : 0.8 * Math.PI / 12,
+                displayThickness : 0.04,
+                resolution : 12
+            },
+            {
+                step : 0.7 * Math.PI / 12,
+                wave : this.waveFunc,
+                amplitude : 0.22,
+                startAngle : null,
+                radius : 0.8,
+                arcLength : 0.62 * Math.PI / 12,
+                displayThickness : 0.05,
+                resolution : 10
+            },
+            {
+                step : 0.65 * Math.PI / 12,
+                wave : this.waveFuncNeg,
+                amplitude : 0.2,
+                startAngle : null,
+                radius : 0.8,
+                arcLength : 0.54 * Math.PI / 12,
+                displayThickness : 0.05,
+                resolution : 8
+            },
+            {
+                step : 0.6 * Math.PI / 12,
+                wave : this.waveFunc,
+                amplitude : 0.19,
+                startAngle : null,
+                radius : 0.42,
+                arcLength : 0.48 * Math.PI / 12,
+                displayThickness : 0.06,
+                resolution : 6
+            },
+            {
+                step : 0.8 * Math.PI / 12,
+                wave : this.waveFuncNeg,
+                amplitude : 0.12,
+                startAngle : null,
+                radius : 0.42,
+                arcLength : 0.45 * Math.PI / 12,
+                displayThickness : 0.06,
+                resolution : 4
+            }
+        ]
+    };
+
     constructor() {
         this.render = this.render.bind(this);
     }
@@ -93,7 +246,7 @@ export class VisualizerContainerComponent implements AfterViewInit {
         // this.scene.add(cube);
         this.scene.add(new THREE.AxesHelper(5));
         // this.scene.add(this.createCircle(1.0, 0.05, 32));
-        this.createEQVisualizer();
+        this.testVisualizerCb = this.createEQVisualizer();
     }
 
     private createTestVisualizer_old() {
@@ -125,166 +278,13 @@ export class VisualizerContainerComponent implements AfterViewInit {
     }
 
     private createEQVisualizer() {
-        const waveFunc = x => Math.sin(x * Math.PI);
-        const waveFuncNeg = x => - 4*x + 4*x*x; // 1 - (2*x - 1) * (2*x - 1)
-        const CIRCLES = [
-            {
-                radius : 3.6,
-                thickness : 0.08
-            },
-            {
-                radius : 2.6,
-                thickness : 0.065
-            },
-            {
-                radius : 1.8,
-                thickness : 0.045
-            },
-            {
-                radius : 1.25,
-                thickness : 0.04
-            },
-            {
-                radius : 0.8,
-                thickness : 0.04
-            },
-            {
-                radius : 0.42,
-                thickness : 0.04
-            }
-        ];
-        const WAVES = {
-            freq : [
-                {
-                    step : 6.0 * Math.PI / 12 + 0.2,
-                    wave : waveFunc,
-                    amplitude : 0.72,
-                    startAngle : null,
-                    radius : 3.6,
-                    arcLength : 6.6 * Math.PI / 12,
-                    displayThickness : 0.08,
-                    resolution : 32
-                },
-                {
-                    step : 2.8 * Math.PI / 12 - 0.04,
-                    wave : waveFuncNeg,
-                    amplitude : 0.56,
-                    startAngle : null,
-                    radius : 3.6,
-                    arcLength : 2.8 * Math.PI / 12,
-                    displayThickness : 0.07,
-                    resolution : 32
-                },
-                {
-                    step : 4.1 * Math.PI / 12 - 0.12,
-                    wave : waveFunc,
-                    amplitude : 0.48,
-                    startAngle : null,
-                    radius : 2.6,
-                    arcLength : 4.1 * Math.PI / 12,
-                    displayThickness : 0.06,
-                    resolution : 32
-                },
-                {
-                    step : 2.5 * Math.PI / 12,
-                    wave : waveFuncNeg,
-                    amplitude : 0.42,
-                    startAngle : null,
-                    radius : 2.6,
-                    arcLength : 2.5 * Math.PI / 12,
-                    displayThickness : 0.05,
-                    resolution : 32
-                },
-                {
-                    step : 2.8 * Math.PI / 12,
-                    wave : waveFunc,
-                    amplitude : 0.45,
-                    startAngle : null,
-                    radius : 1.8,
-                    arcLength : 2.8 * Math.PI / 12,
-                    displayThickness : 0.04,
-                    resolution : 24
-                },
-                {
-                    step : 1.5 * Math.PI / 12,
-                    wave : waveFuncNeg,
-                    amplitude : 0.32,
-                    startAngle : null,
-                    radius : 1.8,
-                    arcLength : 1.5 * Math.PI / 12,
-                    displayThickness : 0.04,
-                    resolution : 24
-                },
-                {
-                    step : 1.2 * Math.PI / 12,
-                    wave : waveFunc,
-                    amplitude : 0.3,
-                    startAngle : null,
-                    radius : 1.25,
-                    arcLength : 1.2 * Math.PI / 12,
-                    displayThickness : 0.04,
-                    resolution : 16
-                },
-                {
-                    step : 0.85 * Math.PI / 12,
-                    wave : waveFuncNeg,
-                    amplitude : 0.27,
-                    startAngle : null,
-                    radius : 1.25,
-                    arcLength : 0.8 * Math.PI / 12,
-                    displayThickness : 0.04,
-                    resolution : 12
-                },
-                {
-                    step : 0.7 * Math.PI / 12,
-                    wave : waveFunc,
-                    amplitude : 0.22,
-                    startAngle : null,
-                    radius : 0.8,
-                    arcLength : 0.62 * Math.PI / 12,
-                    displayThickness : 0.05,
-                    resolution : 10
-                },
-                {
-                    step : 0.65 * Math.PI / 12,
-                    wave : waveFuncNeg,
-                    amplitude : 0.2,
-                    startAngle : null,
-                    radius : 0.8,
-                    arcLength : 0.54 * Math.PI / 12,
-                    displayThickness : 0.05,
-                    resolution : 8
-                },
-                {
-                    step : 0.6 * Math.PI / 12,
-                    wave : waveFunc,
-                    amplitude : 0.19,
-                    startAngle : null,
-                    radius : 0.42,
-                    arcLength : 0.48 * Math.PI / 12,
-                    displayThickness : 0.06,
-                    resolution : 6
-                },
-                {
-                    step : 0.8 * Math.PI / 12,
-                    wave : waveFuncNeg,
-                    amplitude : 0.12,
-                    startAngle : null,
-                    radius : 0.42,
-                    arcLength : 0.45 * Math.PI / 12,
-                    displayThickness : 0.06,
-                    resolution : 4
-                }
-            ]
-        };
-
         var circles: Array<THREE.Mesh> = [
-            this.createCircle(CIRCLES[0].radius, CIRCLES[0].thickness, 256),
-            this.createCircle(CIRCLES[1].radius, CIRCLES[1].thickness, 128),
-            this.createCircle(CIRCLES[2].radius, CIRCLES[2].thickness, 128),
-            this.createCircle(CIRCLES[3].radius, CIRCLES[3].thickness, 64),
-            this.createCircle(CIRCLES[4].radius, CIRCLES[4].thickness, 64),
-            this.createCircle(CIRCLES[5].radius, CIRCLES[5].thickness, 32)
+            this.createCircle(this.CIRCLES[0].radius, this.CIRCLES[0].thickness, 256),
+            this.createCircle(this.CIRCLES[1].radius, this.CIRCLES[1].thickness, 128),
+            this.createCircle(this.CIRCLES[2].radius, this.CIRCLES[2].thickness, 128),
+            this.createCircle(this.CIRCLES[3].radius, this.CIRCLES[3].thickness, 64),
+            this.createCircle(this.CIRCLES[4].radius, this.CIRCLES[4].thickness, 64),
+            this.createCircle(this.CIRCLES[5].radius, this.CIRCLES[5].thickness, 32)
         ];
 
         circles.forEach(circle => { this.scene.add(circle); });
@@ -306,61 +306,32 @@ export class VisualizerContainerComponent implements AfterViewInit {
             return returnArray;
         }
 
-        WAVES.freq.forEach(freq => {
-            range(freq.step)
-                .map(startAngle => this.createWave(freq.wave, freq.amplitude, startAngle, freq.radius, freq.arcLength, freq.displayThickness, freq.resolution))
-                .forEach(wave => this.scene.add(wave));
-        });
+        var component = this;
+        var waves = [];
 
-        //(waveFunc: Function, amplitude: number, startAngle: number, radius: number, arcLength: number, displayThickness: number = 0.02, resolution: number = 32): THREE.Mesh
+        function createAllWaves(rotation, freqValues) {
+            component.WAVES.freq.forEach((freq, index) => {
+                var freqWavesArray = [];
+                waves.push(freqWavesArray);
+                range(freq.step)
+                    .map(startAngle => {
+                        var mesh = component.createWave(freq.wave, freqValues[index] * freq.amplitude, startAngle + rotation, freq.radius, freq.arcLength, freq.displayThickness, freq.resolution);
+                        freqWavesArray.push(mesh);
+                        return mesh;
+                    })
+                    .forEach(wave => component.scene.add(wave));
+            });
+        }
+        createAllWaves(0.0, [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 0.5]);
 
-        // var lowestFreq = range(6.0 * Math.PI / 12 + 0.2);
-        // lowestFreq = lowestFreq.map(startAngle => this.createWave(waveFunc, 0.72, startAngle, 3.6, 6.6 * Math.PI / 12, 0.08, 32));
-        // lowestFreq.forEach(x => this.scene.add(x));
-
-        // var secondLowestFreq = range(2.8 * Math.PI / 12 - 0.04);
-        // secondLowestFreq = secondLowestFreq.map(startAngle => this.createWave(waveFuncNeg, 0.56, startAngle, 3.6, 2.8 * Math.PI / 12, 0.07, 32));
-        // secondLowestFreq.forEach(x => this.scene.add(x));
-
-        // var thirdLowestFreq = range(4.1 * Math.PI / 12 - 0.12);
-        // thirdLowestFreq = thirdLowestFreq.map(startAngle => this.createWave(waveFunc, 0.48, startAngle, 2.6, 4.1 * Math.PI / 12, 0.06, 32));
-        // thirdLowestFreq.forEach(x => this.scene.add(x));
-
-        // var fourthLowestFreq = range(2.5 * Math.PI / 12);
-        // fourthLowestFreq = fourthLowestFreq.map(startAngle => this.createWave(waveFuncNeg, 0.42, startAngle, 2.6, 2.5 * Math.PI / 12, 0.05, 32));
-        // fourthLowestFreq.forEach(x => this.scene.add(x));
-
-        // var fifthLowestFreq = range(2.8 * Math.PI / 12);
-        // fifthLowestFreq = fifthLowestFreq.map(startAngle => this.createWave(waveFunc, 0.45, startAngle, 1.8, 2.8 * Math.PI / 12, 0.04, 24));
-        // fifthLowestFreq.forEach(x => this.scene.add(x));
-
-        // var sixthLowestFreq = range(1.5 * Math.PI / 12);
-        // sixthLowestFreq = sixthLowestFreq.map(startAngle => this.createWave(waveFuncNeg, 0.32, startAngle, 1.8, 1.5 * Math.PI / 12, 0.04, 24));
-        // sixthLowestFreq.forEach(x => this.scene.add(x));
-
-        // var seventhLowestFreq = range(1.2 * Math.PI / 12);
-        // seventhLowestFreq = seventhLowestFreq.map(startAngle => this.createWave(waveFunc, 0.3, startAngle, 1.25, 1.2 * Math.PI / 12, 0.04, 16));
-        // seventhLowestFreq.forEach(x => this.scene.add(x));
-
-        // var eighthLowestFreq = range(0.85 * Math.PI / 12);
-        // eighthLowestFreq = eighthLowestFreq.map(startAngle => this.createWave(waveFuncNeg, 0.27, startAngle, 1.25, 0.8 * Math.PI / 12, 0.04, 12));
-        // eighthLowestFreq.forEach(x => this.scene.add(x));
-
-        // var ninethLowestFreq = range(0.7 * Math.PI / 12);
-        // ninethLowestFreq = ninethLowestFreq.map(startAngle => this.createWave(waveFunc, 0.22, startAngle, 0.8, 0.62 * Math.PI / 12, 0.05, 10));
-        // ninethLowestFreq.forEach(x => this.scene.add(x));
-
-        // var tenthLowestFreq = range(0.65 * Math.PI / 12);
-        // tenthLowestFreq = tenthLowestFreq.map(startAngle => this.createWave(waveFuncNeg, 0.2, startAngle, 0.8, 0.54 * Math.PI / 12, 0.05, 8));
-        // tenthLowestFreq.forEach(x => this.scene.add(x));
-
-        // var eleventhLowestFreq = range(0.6 * Math.PI / 12);
-        // eleventhLowestFreq = eleventhLowestFreq.map(startAngle => this.createWave(waveFunc, 0.19, startAngle, 0.42, 0.48 * Math.PI / 12, 0.06, 6));
-        // eleventhLowestFreq.forEach(x => this.scene.add(x));
-
-        // var twelthLowestFreq = range(0.8 * Math.PI / 12);
-        // twelthLowestFreq = twelthLowestFreq.map(startAngle => this.createWave(waveFuncNeg, 0.12, startAngle, 0.42, 0.45 * Math.PI / 12, 0.06, 4));
-        // twelthLowestFreq.forEach(x => this.scene.add(x));
+        return function controlCallback(rotation, freqValues) {
+            waves.forEach(n => { n.forEach(m => {
+                component.scene.remove(m);
+                m.geometry.dispose();
+            }); });
+            waves = [];
+            createAllWaves(rotation, freqValues);
+        };
     }
 
     private createWave(waveFunc: Function, amplitude: number, startAngle: number, radius: number, arcLength: number, displayThickness: number = 0.02, resolution: number = 32): THREE.Mesh {
@@ -562,7 +533,7 @@ export class VisualizerContainerComponent implements AfterViewInit {
 
         var component = this;
 
-        function testVisualizer() {
+        function testEQVisualizer() {
             let audio = new Audio("../../assets/ChaseMusic_mixAndMaster.mp3");
             audio.load();
 
@@ -573,7 +544,7 @@ export class VisualizerContainerComponent implements AfterViewInit {
             mediaElementSrc.connect(analyser);
             analyser.connect(audioContext.destination);
 
-            analyser.fftSize = 1024;
+            analyser.fftSize = 512;
 
             var bufferLength = analyser.frequencyBinCount;
             var dataArray = new Uint8Array(bufferLength);
@@ -582,20 +553,23 @@ export class VisualizerContainerComponent implements AfterViewInit {
 
             audio.play();
 
+            var rotation = 0.0;
+
             function updateVisualizer() {
+                rotation += 0.005;
                 analyser.getByteFrequencyData(dataArray);
 
-                var stepSize = bufferLength / 16;
-                var heights = [];
+                var stepSize = Math.floor(bufferLength / 12);
+                var freqValues = [];
 
-                for (var n = 0; n < bufferLength; n += stepSize) {
+                for (var n = bufferLength - stepSize; n >= 0; n -= stepSize) {
                     var value = dataArray[n];
-                    if (value <= 0.5)
-                        value = 0.5
-                    heights.push((value - 0.5) / (10 - 0.5) + 0.5);
+                    if (value <= 0.05)
+                        value = 0.05
+                    freqValues.push((value - 0.05) / (10 - 0.05) + 0.05);
                 }
 
-                component.testVisualizerCb(heights);
+                component.testVisualizerCb(rotation, freqValues);
                 component.render();
 
                 requestAnimationFrame(updateVisualizer);
@@ -604,7 +578,7 @@ export class VisualizerContainerComponent implements AfterViewInit {
             updateVisualizer();
         }
 
-        testVisualizer();
+        testEQVisualizer();
     }
 
     ngAfterViewInit() {
